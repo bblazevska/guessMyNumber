220e1602c86779283f76343505750e68351c991e
kuzeva <kuzeva01@gmail.com>
2021-08-23 16:15:50 +0200

    game logic implemented

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 15;
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', () => {
+   const guess = Number(document.querySelector('.guess').value);
+   console.log(guess, typeof guess);
+
+   if (!guess) {
+      document.querySelector('.message').textContent = '⛔ No number!';
+   } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = '✔ Correct Number!';
+      highscore += score;
+      document.querySelector('.highscore').textContent = highscore;
+   } else if (guess > secretNumber) {
+      if (score > 1) {
+         document.querySelector('.message').textContent = '📈 Too high!';
+         score--;
+      } else {
+         document.querySelector('.message').textContent = '🧨 You lost the game!';
+         
+      }
+      document.querySelector('.score').textContent = score;
+   } else if (guess < secretNumber) {
+      if (score > 1) {
+         document.querySelector('.message').textContent = '📉 Too low!';
+         score--;
+         document.querySelector('.score').textContent = score;
+      } else {
+         document.querySelector('.message').textContent = '🧨 You lost the game!';
+         
+      }
+   }
+});
